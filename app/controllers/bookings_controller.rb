65b14406adfe611f9a73ddca2061f42fc7dc3db0
Fatimah Az-Zahra <azzahrafz@gmail.com>
2017-06-19 13:42:56 +0700

    Form complex

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,37 +4,27 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
-    @passenger = Passenger.find(params[:id])
+    @passengers = Passenger.find(params[:id])
   end
 
   def new
     @flights_pick = Flight.find_by_id(params[:bookings][:flight_id])
+    @seats = params[:bookings][:seats].to_i
     @booking = @flights_pick.bookings.build
-    @seats = params[:seats].to_i
+    @seats.times { @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
     if @booking.save
-      @passenger = @booking.passengers.build
-      @passenger = Passenger.new(
-        first_name: params[:bookings][:passengers][:first_name],
-        last_name: params[:bookings][:passengers][:last_name],
-        email: params[:bookings][:passengers][:email],
-        booking_id: @booking.id,
-        flight_id: params[:bookings][:passengers][:flight_id]
-      )
-      @passenger.save
       redirect_to @booking
     else
       redirect_to flights_path
     end
-    binding.pry
   end
 
   private
     def booking_params
-      params.require(:bookings).permit(:flight_id, :passengers_attributes =>
-                                      [:id, :first_name, :last_name, :email, :booking_id, :flight_id])
+      params.require(:bookings).permit(:flight_id, passengers_attributes: [:id, :first_name, :last_name, :email, :booking_id, :flight_id])
     end
 end
